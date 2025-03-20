168964805522788f8fbb462c094908257bbb8d22
k4rth1k315ht <karthikbisht506@gmail.com>
2025-03-20 21:41:16 +0530

    assignment submitted 12345

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,37 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+
+        Product p = new Product();
+
+        System.out.println(p.product(5,2));
+
+        System.out.println(p.product(1,2,3));
+
+        System.out.println(p.product(5.22,5.22));
+
+    }
+
+
+    public static class Product{
+
+        public int product(int x,int y)
+        {
+           return x*y;
+        }
+
+        public int product(int x, int y, int z)
+        {
+          return x*y*z;
+        }
+
+        public double product(double x, double y)
+        {
+         return x*y;
+        }
+
+
+    }
 
 }
